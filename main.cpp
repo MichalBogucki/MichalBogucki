4fc4fdf66de3955e5cf4def9419df3ddf3aac143
mbogucki <mbogucki@lab09.elka.pw.edu.pl>
2017-03-08 15:16:13 +0100

    My first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    string name, surname, nickname,output;
+    cout << "Write name: ";
+    cin >> name;
+    cout << "Write surname: ";
+    cin >> surname;
+    cout << "Write nickname: ";
+    cin >> nickname;
+    cout << endl <<"Output: " << name <<" " << nickname << " " << surname << " " << endl << endl << endl;
+
+    
+    return 0;
+}//int main()
+
+
+
+
+
+
